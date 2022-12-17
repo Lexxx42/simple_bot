d6516e7ae5925f09d692239bd63000eb95eae731
Alexander Konukhov <jesusmisha@gmail.com>
2022-12-17 13:52:34 +0300

    init

---
+++ bot_commands.py
@@ -0,0 +1,30 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import datetime
+from spy import *
+
+
+async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await log(update, context)
+    await update.message.reply_text(f'hello, {update.effective_user.first_name}')
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await log(update, context)
+    await update.message.reply_text(f'/hello\n/time\n/help')
+
+
+async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await log(update, context)
+    await update.message.reply_text(f'{datetime.datetime.now().time()}')
+
+
+async def sum_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await log(update, context)
+    msg = update.message.text
+    print(msg)
+    items = msg.split()  # /sum 123 23
+    x = int(items[1])
+    y = int(items[2])
+
+    await update.message.reply_text(f'{x} + {y} = {x + y}')
---
+++ main.py
@@ -0,0 +1,10 @@
+from bot_commands import *
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("hello", hi_command))
+app.add_handler(CommandHandler("time", time_command))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("sum", sum_command))
+
+app.run_polling()
